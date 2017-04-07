ae284ee8e593141c09671f055cb8c652fcdf8dca
mweav <mzw@cs.princeton.edu>
2017-04-07 14:36:02 -0400

    Added file

---
+++ UniMath/CategoryTheory/monad_representations_equiv.v
@@ -0,0 +1,22 @@
+(** **********************************************************
+
+ Matthew Weaver, 2017
+
+ ************************************************************)
+
+
+(** **********************************************************
+
+ Contents : Equivalence of the traditional defintion of monads
+            to those given in Kleisli form
+
+ ************************************************************)
+
+Require Import UniMath.MoreFoundations.Tactics
+        UniMath.Foundations.Sets
+        UniMath.CategoryTheory.precategories
+        UniMath.CategoryTheory.functor_categories.
+
+Section monad_representations_equiv.
+
+End monad_representations_equiv.
